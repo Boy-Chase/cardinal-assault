317cb2f6925d625d0739409653f7f6c850eecc80
JohnW1455 <jhw5517@g.rit.edu>
2023-03-02 19:10:35 -0500

    Better hovering

---
+++ Cardinal Assault/Assets/Scripts/HoverScript.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoverScript : MonoBehaviour
+{
+    public bool isHover = false;
+    private float scale = 1;
+    public float scaleMax = 1.1f;
+    public float timeIn = 1f;
+    public float timeOut = 2f;
+
+    public void Update()
+    {
+        float delta = Time.unscaledDeltaTime;
+
+        // Hover animation
+        RectTransform rectTransform = this.gameObject.GetComponent<RectTransform>();
+        if (isHover && scale < scaleMax)
+        {
+            scale += timeIn * delta;
+            if (scale > scaleMax)
+            {
+                scale = scaleMax;
+            }
+        }
+        else if (!isHover && scale > 1f)
+        {
+            scale -= timeOut * delta;
+            if (scale < 1f)
+            {
+                scale = 1f;
+            }
+            
+        }
+        rectTransform.localScale = new Vector3(scale, scale, 1);
+    }
+
+    public void OnHoverStart()
+    {
+        isHover = true;
+    }
+
+    public void OnHoverStop()
+    {
+        isHover = false;
+    }
+
+    public void OnDisable()
+    {
+        scale = 1f;
+        isHover = false;
+    }
+}
+
