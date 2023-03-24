ef91b2923ea23edb5733e9c8a34eabdbb540544f
Boy-Chase <70419012+Boy-Chase@users.noreply.github.com>
2023-03-24 13:50:25 -0400

    started reverse enemy

--- Cardinal Assault/Assets/Scripts/Player.cs
+++ Cardinal Assault/Assets/Scripts/Player.cs
@@ -143,6 +143,38 @@ public class Player : MonoBehaviour
 
         LevelManager.Instance.RemoveEnemy(collision.gameObject);
 
+        /*
+        if (collision.GetComponentInParent<ScriptableObject>().name == "Opposite")
+        {
+            if (collision.IsTouching(transform.GetComponent<BoxCollider2D>()))
+            {
+                //AudioSource.PlayClipAtPoint(blockSound, gameObject.transform.position);
+                block.Play();
+                streak++;
+                if (highestStreak < streak)
+                {
+                    highestStreak = streak;
+                }
+                streakNum.SetText(streak.ToString());
+                streakNum.color = new Color(1, 1 - (streak / 10), 1 - (streak / 10));
+            }
+            else
+            {
+                health--;
+                healthBar.setHealth(health);
+                Debug.Log($"Player got hit! Health: {health}");
+                AudioSource.PlayClipAtPoint(hitSound, gameObject.transform.position);
+                CameraEffects.Instance.Hurt();
+                hurt.Play();
+                if (!isHurt) isHurt = !isHurt;
+                streak = 0;
+                if (streakGrade > 0) streakGrade--;
+                streakNum.SetText(streak.ToString());
+                streakNum.color = new Color(1, 1, 1);
+            }
+        }
+        else { }*/
+
         if (collision.IsTouching(transform.GetComponent<BoxCollider2D>())) {    
             health--;
             healthBar.setHealth(health);
